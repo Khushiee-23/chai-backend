27d0af0cc66f5c425c11341be48a2ce3f13277cf
Khushiee-23 <kk2305singh@gmail.com>
2025-09-27 20:22:52 +0530

    Fix MongoDB connection and .env variable names

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constant.js";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({ path: './.env' });
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'dotenv/config';
+
+// import express from "express"
+// const app = express();
+// (async() => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGOOSE_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("ERRR: ", error);
+//             throw error
+//         })
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.error("ERROR: ", error);
+//         throw error;
+//     }
+
+// })()
